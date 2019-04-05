5a1ecec59eb3fb4ca6196de34be4d129bdf3cccb
Caleb Einarr <earthctzn1@gmail.com>
2019-04-05 05:28:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -30,7 +30,7 @@ def retrieve_element_from_index(array, [])
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
 end
 
 def retrieve_last_element_from_array(array)
